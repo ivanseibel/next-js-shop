5bb4b3a940bc95c462f7150b97801310d532b937
Ivan Seibel <me@ivanseibel.dev>
2022-12-19 21:23:31 +0000

    replace getServerSideProps by getStaticProps

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import Stripe from "stripe"
 import { stripe } from "../lib/stripe"
 import Image from "next/image"
-import { GetServerSideProps } from "next"
+import { GetStaticProps } from "next"
 import {useKeenSlider} from "keen-slider/react"
 
 import { HomeContainer, Product } from "../styles/pages/home"
@@ -49,7 +49,7 @@ export default function Home({ products }: HomeProps) {
   )
 }
 
-export const getStaticProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const response = await stripe.products.list({
     expand: ['data.default_price']
   })
