d5871427d26ac3294e243d46db6c3080f7d1271f
Ivan Seibel <me@ivanseibel.dev>
2022-12-20 22:28:25 +0000

    creates checkout route

---
+++ src/pages/api/checkout.ts
@@ -0,0 +1,30 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { stripe } from "../../lib/stripe";
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const { priceId } = req.body;
+  const successUrl = `${process.env.NEXT_URL}/success`
+  const cancelUrl = `${process.env.NEXT_URL}/`
+
+  if (req.method !== "POST") {
+    return res.setHeader("Allow", "POST").status(405).end("Method not allowed");
+  }
+
+  if (!priceId) {
+    return res.status(400).json({ message: "Price ID is required" });
+  }
+  
+  const checkoutSession = await stripe.checkout.sessions.create({
+    mode: "payment",
+    line_items: [
+      {
+        price: priceId,
+        quantity: 1,
+      }
+    ],
+    success_url: successUrl,
+    cancel_url: cancelUrl,
+  });
+
+  return res.status(201).json({ checkoutUrl: checkoutSession.url });
+}
