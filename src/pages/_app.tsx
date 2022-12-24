f2f737cc3cbee143bb8676251edb2957f41cfcd7
Ivan Seibel <me@ivanseibel.dev>
2022-12-24 20:22:48 +0000

    creates basic dialog

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,6 +5,7 @@ import logo from '../assets/logo.svg'
 import Image from 'next/image';
 import { Container, Header, BagContainer } from '../styles/pages/app';
 import { Handbag } from 'phosphor-react';
+import * as Dialog from '@radix-ui/react-dialog';
 
 globalStyles();
 
@@ -15,10 +16,22 @@ export default function App({ Component, pageProps }: AppProps) {
       <Header>
         <div>
         <Image src={logo} alt="" />
-        <BagContainer>
-          <Handbag size={24} color={numberOfItems > 0 ? '#fff' : '#8D8D99'} />
-          {numberOfItems > 0 && <span>{numberOfItems}</span>}
-        </BagContainer>
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <BagContainer>
+              <Handbag size={24} color={numberOfItems > 0 ? '#fff' : '#8D8D99'} />
+              {numberOfItems > 0 && <span>{numberOfItems}</span>}
+            </BagContainer>
+          </Dialog.Trigger>
+          <Dialog.Portal>
+            <Dialog.Overlay />
+            <Dialog.Content>
+              <Dialog.Title />
+              <Dialog.Description />
+              <Dialog.Close />
+            </Dialog.Content>
+          </Dialog.Portal>
+        </Dialog.Root>
         </div>
       </Header>
       <Component {...pageProps} />
