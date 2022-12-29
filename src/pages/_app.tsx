a0944093061c758c5f9de3a2fd789e83cdae3c0c
Ivan Seibel <me@ivanseibel.dev>
2022-12-29 19:38:09 +0000

    centers logo if success page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,16 +5,19 @@ import logo from '../assets/logo.svg'
 import Image from 'next/image';
 import { Container, Header } from '../styles/pages/app';
 import { Cart } from '../components/Cart';
+import { useRouter } from 'next/router';
 
 globalStyles();
 
 export default function App({ Component, pageProps }: AppProps) {
+  const { pathname } = useRouter()
+
   return (
     <Container>
-      <Header>
+      <Header centered={pathname === '/success'} >
         <div>
-        <Image src={logo} alt="" />
-        <Cart />
+          <Image src={logo} alt="" />
+          <Cart />
         </div>
       </Header>
       <Component {...pageProps} />
