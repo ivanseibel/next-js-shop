eedff05137e03a15278d9e52f2c60529606251de
Ivan Seibel <me@ivanseibel.dev>
2023-01-16 23:49:41 +0000

    add item to cart

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Stripe from "stripe"
+import { useShoppingCart } from "use-shopping-cart"
 import { stripe } from "../lib/stripe"
 import Head from "next/head"
 import Image from "next/image"
@@ -9,23 +10,41 @@ import { BagContainer, HomeContainer, Product } from "../styles/pages/home"
 import 'keen-slider/keen-slider.min.css'
 import Link from "next/link"
 import { Handbag } from "phosphor-react"
+import { Description } from '../components/Cart/styles';
+
+interface Product {
+  id: string
+  name: string
+  description: string
+  imageUrl: string
+  price: string
+  priceAsNumber: number
+}
 
 interface HomeProps {
-  products: {
-    id: string
-    name: string
-    imageUrl: string
-    price: string
-  }[]
+  products: Product[]
 }
 
 export default function Home({ products }: HomeProps) {
+  const { addItem } = useShoppingCart()
+
   const [sliderRef] = useKeenSlider({
     slides: {
       perView: 3,
       spacing: 48,
     }
   })
+
+  function handleAddItem(product: Product) {
+    addItem({
+      id: product.id,
+      name: product.name,
+      description: product.description,
+      price: product.priceAsNumber,
+      currency: 'EUR',
+      image: product.imageUrl
+    })
+  }
 
   return (
     <>
@@ -35,7 +54,7 @@ export default function Home({ products }: HomeProps) {
 
       <HomeContainer ref={sliderRef} className="keen-slider">
         {products.map(product => (
-          <Link key={product.id} href={`/product/${product.id}`}>
+          <Link key={product.id} href="#" onClick={() => handleAddItem(product)} >
             <Product className="keen-slider__slide">
               <Image src={product.imageUrl} width={520} height={480} alt="" />
 
@@ -72,11 +91,13 @@ export const getStaticProps: GetStaticProps = async () => {
     return {
       id: product.id,
       name: product.name,
+      description: product.description,
       imageUrl: product.images[0],
       price: new Intl.NumberFormat('en-IE', {
         style: 'currency',
         currency: 'EUR',
-      }).format(price.unit_amount! / 100)
+      }).format(price.unit_amount! / 100),
+      priceAsNumber: price.unit_amount! / 100
     }
   })
 
