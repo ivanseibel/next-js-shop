8445a54a942a9826319387eabed46b72a82ee66d
Ivan Seibel <me@ivanseibel.dev>
2023-02-22 22:28:35 +0000

    downgrade stripe version

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -3,7 +3,7 @@ import Stripe from 'stripe';
 const STRIPE_SECRET_KEY = process.env.STRIPE_SECRET_KEY || '';
 
 export const stripe = new Stripe(STRIPE_SECRET_KEY, {
-  apiVersion: '2022-11-15',
+  apiVersion: '2022-08-01',
   appInfo: {
     name: 'Ignite Shop',
   },
